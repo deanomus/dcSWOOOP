b6b48b6441c2acdc235e51e6f3b62e1338357567
Dean Reinhardt <reinhardt@myswooop.de>
2019-09-25 14:38:31 +0200

    Bot send now a ,essage to #mod-log channel, when new channel are created

--- src/main/java/de/deanomus/dc/listener/TextChannelCreate.java
+++ src/main/java/de/deanomus/dc/listener/TextChannelCreate.java
@@ -15,6 +15,8 @@ public class TextChannelCreate extends ListenerAdapter {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         e.getChannel().sendMessage(Embed.success.setDescription("Dieser Channel sieht aber sehr toll aus :smile: " + e.getChannel().getAsMention()).setFooter("Created at: " + formatter.format(date)).build()).queue();
+        e.getJDA().getGuildById(Data.DC_GUILD).getTextChannelById(Data.DC_MOD_LOG).sendMessage(Embed.success.setDescription("Ein neuer Channel wurde erstellt :white_check_mark: " + e.getChannel().getAsMention()).build()).queue();
+        Embed.success.setFooter(null);
     }
 
 
--- src/main/java/de/deanomus/dc/storage/Data.java
+++ src/main/java/de/deanomus/dc/storage/Data.java
@@ -19,6 +19,7 @@ public class Data {
             DC_TOKEN = "NjIxNjMyMjgyNTE5OTI4ODMz.XXoKrg._t1OBUCBFaTBoPD-AwQys-N7lGY",
             DC_GUILD = "621633248967131136",
             DC_BOT_LOG = "621640962271215636",
+            DC_MOD_LOG = "625973361973133322",
             DC_USER_LOG = "621671553847001088",
             DC_CMD_PREFIX = "-",
             StatsKey = "T^y2sWVksduH8^9o!tLrktH^PbL&bpA0@S2Eog7sVBwyxXrG04g^hya8TMb5rluj@l#7FrkN2Be2VeywK0JhoS*!yXwGuCSv@FlVZUcmBBLU%gLy3DYCp6%jsDD#3dX0VgJVVs8PQ#Qw9rkhiW5rFYWVF!im2^$JscHgMbDEGa%HnNsvfL5ZE46o3hW!utbDyffLk8#x8KJ782Ns9nKPqtjQwu2vSqEsc48xhavh&QePMLQllaioV@dL4JCYKbmTQ5EkaXd1YU%EqqJ#58Bttf1R6p3965S6U2S@WJmj!SqSTe&sU08zl3S5DA8xcB26U@6CFf@ijFQYpH^!G&IaM^KX$$Ny0!4hhhe7K51ej$UaFrYNPjK3*nZdMhiZVHqX2ypQ5oouyFhoV2rHyg0UC%73zaaPJTTGLCZysekgzdU5!1OzLnlV!nfmFrlzPT7xFM11YBSYVEJKA0mMMbxh35r*PQm9c&WjNme1MkT27uV0p2Ghi!CE",
