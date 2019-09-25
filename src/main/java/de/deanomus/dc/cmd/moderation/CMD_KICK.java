dd5d9bf33b3d1c60928a1ced7fbf040167cc9c2c
Dean Reinhardt <reinhardt@myswooop.de>
2019-09-25 11:47:28 +0200

    new classes for more moderations commands

---
+++ src/main/java/de/deanomus/dc/cmd/moderation/CMD_KICK.java
@@ -0,0 +1,26 @@
+package de.deanomus.dc.cmd.moderation;
+
+import de.deanomus.dc.cmd.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class CMD_KICK implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+}
